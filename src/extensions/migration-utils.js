b6372132fe1077880d47508244bd8391d52a81ee
Alexey Yarmosh <yarmoshalexey@gmail.com>
2024-07-29 13:47:40 +0200

    feat: add migration utils file

---
+++ src/extensions/migration-utils.js
@@ -0,0 +1,57 @@
+const DIRECTUS_URL = process.env.DIRECTUS_URL;
+const ADMIN_ACCESS_TOKEN = process.env.ADMIN_ACCESS_TOKEN;
+const USER_ROLE_NAME = 'User';
+
+async function getUserRoleId () {
+	const URL = `${DIRECTUS_URL}/roles?filter[name][_eq]=${USER_ROLE_NAME}&access_token=${ADMIN_ACCESS_TOKEN}`;
+	const response = await fetch(URL).then((response) => {
+		if (!response.ok) {
+			throw new Error(`Fetch request failed. Status: ${response.status}`);
+		}
+
+		return response.json();
+	});
+	return response.data[0].id;
+}
+
+export async function getUserPermissions (collectionName) {
+	const userRoleId = await getUserRoleId();
+	const URL = `${DIRECTUS_URL}/permissions?filter[collection][_eq]=${collectionName}&filter[role][_eq]=${userRoleId}&access_token=${ADMIN_ACCESS_TOKEN}`;
+	const response = await fetch(URL).then((response) => {
+		if (!response.ok) {
+			throw new Error(`Fetch request failed. Status: ${response.status}`);
+		}
+
+		return response.json();
+	});
+	const permissions = response.data;
+	const readPermissions = permissions.find(({ action }) => action === 'read');
+
+	return { readPermissions };
+}
+
+export async function updatePermissions (readPermissions, fieldsToAdd, fieldsToRemove) {
+	const URL = `${DIRECTUS_URL}/permissions/${readPermissions.id}?access_token=${ADMIN_ACCESS_TOKEN}`;
+	const filteredFields = readPermissions.fields.filter(field => !fieldsToRemove.includes(field));
+
+	const response = await fetch(URL, {
+		method: 'PATCH',
+		body: JSON.stringify({
+			...readPermissions,
+			fields: [
+				...filteredFields,
+				...fieldsToAdd,
+			],
+		}),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	}).then((response) => {
+		if (!response.ok) {
+			throw new Error(`Fetch request failed. Status: ${response.status}`);
+		}
+
+		return response.json();
+	});
+	return response.data;
+}
--- src/extensions/migrations/20240701GP-add-permissions-to-read-credits-user-id.js
+++ src/extensions/migrations/20240701GP-add-permissions-to-read-credits-user-id.js
@@ -1,68 +1,12 @@
-const DIRECTUS_URL = process.env.DIRECTUS_URL;
-const ADMIN_ACCESS_TOKEN = process.env.ADMIN_ACCESS_TOKEN;
-const USER_ROLE_NAME = 'User';
+import { getUserPermissions, updatePermissions } from '../migration-utils.js';
 
 const COLLECTION_NAME = 'gp_credits';
-const FIELDS_TO_REMOVE = [];
 const FIELDS_TO_ADD = [ 'user_id' ];
-
-async function getUserRoleId () {
-	const URL = `${DIRECTUS_URL}/roles?filter[name][_eq]=${USER_ROLE_NAME}&access_token=${ADMIN_ACCESS_TOKEN}`;
-	const response = await fetch(URL).then((response) => {
-		if (!response.ok) {
-			throw new Error(`Fetch request failed. Status: ${response.status}`);
-		}
-
-		return response.json();
-	});
-	return response.data[0].id;
-}
-
-async function getUserPermissions (roleId) {
-	const URL = `${DIRECTUS_URL}/permissions?filter[collection][_eq]=${COLLECTION_NAME}&filter[role][_eq]=${roleId}&access_token=${ADMIN_ACCESS_TOKEN}`;
-	const response = await fetch(URL).then((response) => {
-		if (!response.ok) {
-			throw new Error(`Fetch request failed. Status: ${response.status}`);
-		}
-
-		return response.json();
-	});
-	const permissions = response.data;
-	const readPermissions = permissions.find(({ action }) => action === 'read');
-
-	return { readPermissions };
-}
-
-async function patchReadPermissions (readPermissions) {
-	const URL = `${DIRECTUS_URL}/permissions/${readPermissions.id}?access_token=${ADMIN_ACCESS_TOKEN}`;
-	const filteredFields = readPermissions.fields.filter(field => !FIELDS_TO_REMOVE.includes(field));
-
-	const response = await fetch(URL, {
-		method: 'PATCH',
-		body: JSON.stringify({
-			...readPermissions,
-			fields: [
-				...filteredFields,
-				...FIELDS_TO_ADD,
-			],
-		}),
-		headers: {
-			'Content-Type': 'application/json',
-		},
-	}).then((response) => {
-		if (!response.ok) {
-			throw new Error(`Fetch request failed. Status: ${response.status}`);
-		}
-
-		return response.json();
-	});
-	return response.data;
-}
+const FIELDS_TO_REMOVE = [];
 
 export async function up () {
-	const roleId = await getUserRoleId();
-	const { readPermissions } = await getUserPermissions(roleId);
-	await patchReadPermissions(readPermissions);
+	const { readPermissions } = await getUserPermissions(COLLECTION_NAME);
+	await updatePermissions(readPermissions, FIELDS_TO_ADD, FIELDS_TO_REMOVE);
 	console.log(`User read permissions patched. Added ${FIELDS_TO_ADD.join(',')} to the ${COLLECTION_NAME}`);
 }
 
