77f061cfee1cb2e736a8301d178ef8108eacc042
Alex Yarmosh <yarmoshalexey@gmail.com>
2024-08-13 20:41:30 +0200

    feat: address FE TODO comments (#16)

--- src/extensions/endpoints/credits-timeline/src/index.ts
+++ src/extensions/endpoints/credits-timeline/src/index.ts
@@ -62,24 +62,7 @@ export default defineEndpoint((router, context) => {
 					),
 			]).orderBy('date_created', 'desc').limit(query.limit).offset(query.offset) as CreditsChange[];
 
-			const firstChange = changes[changes.length - 1];
-
-			let [ [{ totalAdditions }], [{ totalDeductions }] ] = await Promise.all([
-				database('gp_credits_additions')
-					.join('directus_users', 'gp_credits_additions.github_id', 'directus_users.external_identifier')
-					.sum('gp_credits_additions.amount as totalAdditions')
-					.where('directus_users.id', value.accountability!.user!)
-					.andWhere('gp_credits_additions.date_created', '<', firstChange ? firstChange.date_created : database.raw('NOW()')) as unknown as [{ totalAdditions: number }],
-				database('gp_credits_deductions')
-					.where('user_id', value.accountability!.user!)
-					.sum('amount as totalDeductions')
-					.where('date', '<', firstChange ? firstChange.date_created : database.raw('NOW()')) as unknown as [{ totalDeductions: number }],
-			]);
-
-			totalAdditions = totalAdditions ?? 0;
-			totalDeductions = totalDeductions ?? 0;
-
-			res.send({ amountBeforeChanges: totalAdditions - totalDeductions, changes });
+			res.send({ changes });
 		} catch (error: unknown) {
 			logger.error(error);
 
---
+++ src/extensions/hooks/notifications-format/src/index.ts
@@ -0,0 +1,39 @@
+import { defineHook } from '@directus/extensions-sdk';
+import markdownit from 'markdown-it';
+import sanitizeHtml from 'sanitize-html';
+import type { NextFunction, Request as ExpressRequest, Response } from 'express';
+import type { Notification } from '@directus/types';
+
+export type Request = ExpressRequest & {
+	accountability?: {
+		customParams?: Record<string, unknown>;
+	},
+};
+
+const md = markdownit();
+
+export default defineHook(({ init, action }) => {
+	init('middlewares.after', ({ app }) => {
+		app.use((req: Request, _res: Response, next: NextFunction) => {
+			// Unknown parameters passed to standard endpoints are removed by Directus. So we are passing them in accountability object.
+			if (req.accountability && req.method === 'GET' && req.query.format === 'html') {
+				req.accountability = {
+					...req.accountability,
+					customParams: { format: 'html' },
+				};
+			}
+
+			next();
+		});
+	});
+
+	action('notifications.read', ({ payload }, { accountability }) => {
+		if ((accountability as Request['accountability'])?.customParams?.format === 'html') {
+			payload.forEach((notification: Notification) => {
+				if (notification.message) {
+					notification.message = sanitizeHtml(md.render(notification.message));
+				}
+			});
+		}
+	});
+});
