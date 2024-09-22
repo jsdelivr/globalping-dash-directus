dda53188fa49c32564f011578235a1c15d7e61fe
Martin Kolárik <martin@kolarik.sk>
2024-09-22 15:59:55 +0200

    fix: allow filtering tokens by app

--- src/extensions/hooks/gp-tokens/src/actions/validate-items-query.ts
+++ src/extensions/hooks/gp-tokens/src/actions/validate-items-query.ts
@@ -1,7 +1,7 @@
 import { createError } from '@directus/errors';
 import _ from 'lodash';
 
-const FilteringError = createError('INVALID_PAYLOAD_ERROR', 'Filtering is not availiable for "gp_tokens" collection', 400);
+const FilteringError = createError('INVALID_PAYLOAD_ERROR', 'Filtering is not available for "gp_tokens" collection', 400);
 
 const getKeysDeep = (entity: object | object[] | string[]) => {
 	const keys = _.isArray(entity) ? [] : Object.keys(entity);
@@ -22,8 +22,8 @@ export const validateQuery = (query: {filter?: object, search?: object} = {}) =>
 		const filterKeys = getKeysDeep(query.filter);
 		const dataFields = _.uniq(filterKeys).filter(key => !key.startsWith('_'));
 
-		if (dataFields.every(field => [ 'id', 'user_created' ].includes(field))) {
-			return; // Filter by "id" is required to not break the Directus UI. And "user_created" for Dashbboard.
+		if (dataFields.every(field => [ 'id', 'user_created', 'app_id' ].includes(field))) {
+			return; // Filter by "id" is required to not break the Directus UI. And "user_created" and "app_id" for Dashboard.
 		}
 
 		throw new FilteringError();
