3d925f4eb8a9da156d91fe02c3b3462ee43c1dd1
Alexey Yarmosh <yarmoshalexey@gmail.com>
2024-11-06 11:41:57 +0100

    feat: start services only if there are no already running

--- test/e2e/global.setup.ts
+++ test/e2e/global.setup.ts
@@ -1,22 +1,50 @@
-import pm2 from 'pm2';
+import pm2, { StartOptions } from 'pm2';
 import { execa } from 'execa';
 import { test as setup } from '@playwright/test';
 import { client as sql } from './utils/client.ts';
+import { promisify } from 'util';
+
+const waitFor = (url: string) => execa`./scripts/wait-for.sh -t 10 ${url}`;
+
+const getIsRunning = async (url: string) => {
+	try {
+		await execa`./scripts/wait-for.sh -t 2 ${url}`;
+		return true;
+	} catch (err) {
+		if (err.stderr === 'Operation timed out') {
+			console.log(`Service at ${url} not running. Starting...`);
+		} else {
+			console.log(err);
+		}
+
+		return false;
+	}
+};
+
+const start = promisify(pm2.start.bind(pm2)) as (options: StartOptions) => Promise<void>;
 
 setup('docker compose start', async () => {
 	await Promise.all([ (async () => {
+		const isDirectusRunning = await getIsRunning('http://localhost:18055');
+
+		if (isDirectusRunning) { return; }
+
 		await execa`docker compose start`;
-		await execa`./scripts/wait-for.sh -t 10 http://localhost:18055/admin/login`;
+		await waitFor('http://localhost:18055');
 	})(), (async () => {
-		await new Promise<void>((resolve, reject) => pm2.start({
+		const isDashRunning = await getIsRunning('http://localhost:13010');
+
+		if (isDashRunning) { return; }
+
+		await start({
 			name: 'dashboard',
 			script: 'test/e2e/globalping-dash/.output/server/index.mjs',
 			env: {
 				PORT: '13010',
 			},
-		}, (err) => { err ? reject(err) : resolve(); }));
+		});
 
-		await execa`./scripts/wait-for.sh -t 10 http://localhost:13010`;
+		await waitFor('http://localhost:13010');
 	})() ]);
 });
 
