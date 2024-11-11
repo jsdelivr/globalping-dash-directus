6523137f616cd08b19218d00d7e45b97fca06645
Alexey Yarmosh <yarmoshalexey@gmail.com>
2024-11-11 16:18:53 +0100

    feat: remove seed dependency from e2e tests

--- test/e2e/global.setup.ts
+++ test/e2e/global.setup.ts
@@ -50,7 +50,21 @@ setup('Start services', async () => {
 });
 
 setup('Init db', async () => {
-	await sql.seed.run();
+	const userRole = await sql('directus_roles').where({ name: 'User' }).select('id').first();
+	await sql('directus_users').upsert([{
+		id: '940d4737-394d-428f-b9d5-d98bf1f2a066',
+		first_name: 'John',
+		last_name: 'Doe',
+		email: 'e2e@example.com',
+		password: '$argon2id$v=19$m=65536,t=3,p=4$UAmnqQvr4aGkytr3SIr68Q$aglm45P0itFgFKfyWyKOgVLXzZvCZHQJJR3geuAZgwU', // password: user
+		role: userRole.id,
+		provider: 'default',
+		external_identifier: '1111111',
+		email_notifications: 1,
+		github_organizations: JSON.stringify([ 'Scrubs' ]),
+		github_username: 'johndoe',
+		user_type: 'sponsor',
+	}]);
 });
 
 setup('Authenticate', async ({ request }) => {
