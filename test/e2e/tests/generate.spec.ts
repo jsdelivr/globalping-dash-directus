51954d8616409fed01880ab544ba51b52dd15e83
Alexey Yarmosh <yarmoshalexey@gmail.com>
2024-11-13 16:03:21 +0100

    feat: add ci workflow

--- test/e2e/tests/generate.spec.ts
+++ test/e2e/tests/generate.spec.ts
@@ -1,5 +1,6 @@
 import { test } from '../fixtures.ts';
 
+// This test is used in `pnpm run test:e2e:generate` to generate new tests.
 test('Mock generate test template', async ({ page }) => {
 	await page.goto('/');
 	await page.pause();
