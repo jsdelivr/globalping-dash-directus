0fa2de233fb55d2668d59d83f720a56e08f6df36
Alexey Yarmosh <yarmoshalexey@gmail.com>
2025-01-02 14:32:06 +0100

    feat: support for e2e tests in globalping-dash repo (#51)

---
+++ scripts/build-dash.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+# Checkout and sync the branch if the directory exists, otherwise clone the repository
+if [ -d "test/e2e/globalping-dash" ]; then
+  cd test/e2e/globalping-dash
+  git add .
+  git reset --hard @{u}
+  git fetch
+  git checkout $CURRENT_BRANCH || git checkout master
+  git reset --hard @{u}
+else
+  git clone -b $CURRENT_BRANCH https://github.com/jsdelivr/globalping-dash.git test/e2e/globalping-dash || git clone https://github.com/jsdelivr/globalping-dash.git test/e2e/globalping-dash
+  cd test/e2e/globalping-dash
+fi
+
+# Install dependencies and build
+pnpm install --ignore-workspace
+dotenv -e ../../../.env -- pnpm build
+
+cd ../../../
