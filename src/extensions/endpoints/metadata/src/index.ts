9489317a72702b2a24bc426b387a968701043dca
Alexey Yarmosh <yarmoshalexey@gmail.com>
2025-01-15 20:23:09 +0100

    feat: make metadata public

--- src/extensions/endpoints/metadata/src/index.ts
+++ src/extensions/endpoints/metadata/src/index.ts
@@ -1,30 +1,11 @@
-import { createError, isDirectusError } from '@directus/errors';
+import { isDirectusError } from '@directus/errors';
 import { defineEndpoint } from '@directus/extensions-sdk';
-import type { EventContext } from '@directus/types';
-import type { Request as ExpressRequest } from 'express';
-import Joi from 'joi';
-
-type Request = ExpressRequest & {
-	accountability: NonNullable<EventContext['accountability']>;
-};
-
-const getMetadataSchema = Joi.object<Request>({
-	accountability: Joi.object({
-		user: Joi.string().required(),
-	}).required().unknown(true),
-}).unknown(true);
 
 export default defineEndpoint((router, context) => {
 	const { logger, env } = context;
 
-	router.get('/', async (req, res) => {
+	router.get('/', async (_req, res) => {
 		try {
-			const { error } = getMetadataSchema.validate(req, { convert: true });
-
-			if (error) {
-				throw new (createError('FORBIDDEN', 'Forbidden', 403))();
-			}
-
 			res.send({
 				targetNodeVersion: env.TARGET_NODE_VERSION,
 				targetHardwareDeviceFirmware: env.TARGET_HW_DEVICE_FIRMWARE,
