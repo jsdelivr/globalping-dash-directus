911cc488d639f5435b9167802f396c1710fa8dbd
Alex Yarmosh <yarmoshalexey@gmail.com>
2025-01-28 14:23:41 +0100

    feat: add public_probes field to user object (#56)

--- src/extensions/migrations/20240701GP-add-permissions-to-read-credits-user-id.js
+++ src/extensions/migrations/20240701GP-add-permissions-to-read-credits-user-id.js
@@ -1,4 +1,4 @@
-import { getUserPermissions, updatePermissions } from '../migration-utils.js';
+import { getUserPermissions, editPermissions } from '../migration-utils.js';
 
 const COLLECTION_NAME = 'gp_credits';
 const FIELDS_TO_ADD = [ 'user_id' ];
@@ -6,7 +6,7 @@ const FIELDS_TO_REMOVE = [];
 
 export async function up () {
 	const { readPermissions } = await getUserPermissions(COLLECTION_NAME);
-	await updatePermissions(readPermissions, FIELDS_TO_ADD, FIELDS_TO_REMOVE);
+	await editPermissions(readPermissions, FIELDS_TO_ADD, FIELDS_TO_REMOVE);
 	console.log(`User read permissions patched. Added ${FIELDS_TO_ADD.join(',')} to the ${COLLECTION_NAME}`);
 }
 
