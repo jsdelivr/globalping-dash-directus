896ac3663f1fd038be06c7586889a0de534a868a
Alexey Yarmosh <yarmoshalexey@gmail.com>
2025-03-03 21:31:11 +0100

    feat: add more e2e tests

--- test/e2e/tests/probes.spec.ts
+++ test/e2e/tests/probes.spec.ts
@@ -4,7 +4,7 @@ import { client as sql } from '../client.ts';
 import { User } from '../types.ts';
 import { randomIP } from '../utils.ts';
 
-const addData = async (user: User) => {
+const addUserProbes = async (user: User) => {
 	const probeId = randomUUID();
 	await sql('gp_probes').insert([{
 		id: probeId,
@@ -59,14 +59,85 @@ const addData = async (user: User) => {
 	}]);
 };
 
+const addProbeWithoutUser = async () => {
+	await sql('gp_probes').insert({
+		id: randomUUID(),
+		ip: '2.2.2.2',
+		uuid: '7bac0b3a-f808-48e1-8892-062bab3280f8',
+		name: null,
+		asn: 3302,
+		city: 'Ouagadougou',
+		country: 'BF',
+		date_created: new Date(),
+		isCustomCity: 0,
+		lastSyncDate: new Date(),
+		latitude: 12.37,
+		longitude: -1.53,
+		network: 'IRIDEOS S.P.A.',
+		onlineTimesToday: 50,
+		state: null,
+		status: 'ready',
+		userId: null,
+		version: '0.28.0',
+		hardwareDevice: null,
+	});
+};
+
+const addProbeWithUser = async (user2: User) => {
+	await sql('gp_probes').insert({
+		id: randomUUID(),
+		ip: '2.2.2.2',
+		uuid: '7bac0b3a-f808-48e1-8892-062bab3280f8',
+		name: null,
+		asn: 3302,
+		city: 'Ouagadougou',
+		country: 'BF',
+		date_created: new Date(),
+		isCustomCity: 0,
+		lastSyncDate: new Date(),
+		latitude: 12.37,
+		longitude: -1.53,
+		network: 'IRIDEOS S.P.A.',
+		onlineTimesToday: 50,
+		state: null,
+		status: 'ready',
+		userId: user2.id,
+		version: '0.28.0',
+		hardwareDevice: null,
+	});
+};
+
 test('Probes page', async ({ page, user }) => {
-	await addData(user);
+	await addUserProbes(user);
 	await page.goto('/probes');
 	await expect(page.locator('h1')).toHaveText('Probes');
 	await expect(page.locator('tr')).toHaveCount(3);
 });
 
 test('Probe adoption', async ({ page }) => {
+	addProbeWithoutUser();
+	await page.goto('/probes');
+	await page.getByRole('button', { name: 'Adopt a probe' }).click();
+	await page.getByLabel('Next step').click();
+	await page.getByPlaceholder('Enter IP address of your probe').fill('2.2.2.2');
+	await page.getByLabel('Send adoption code').click();
+	await page.getByTestId('adoption-code').locator('input').first().fill('111111');
+	await page.getByLabel('Verify the code').click();
+	await page.getByLabel('Finish').click();
+	await expect(page.getByText('probe-bf-ouagadougou-01').first()).toBeVisible();
+});
+
+test('Probe adoption fail if probe is already adopted', async ({ page, user2 }) => {
+	addProbeWithUser(user2);
+	await page.goto('/probes');
+	await page.getByRole('button', { name: 'Adopt a probe' }).click();
+	await page.getByLabel('Next step').click();
+	await page.getByPlaceholder('Enter IP address of your probe').fill('2.2.2.2');
+	await page.getByLabel('Send adoption code').click();
+	await expect(page.getByText('The probe with this IP address is already adopted').first()).toBeVisible();
+});
+
+test('Probe adoption of non-synced probe', async ({ page }) => {
 	await page.goto('/probes');
 	await page.getByRole('button', { name: 'Adopt a probe' }).click();
 	await page.getByLabel('Next step').click();
