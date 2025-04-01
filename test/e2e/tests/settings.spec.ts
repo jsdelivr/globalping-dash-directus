3672adfb736110fa087c2e8c31858a4d5027b3f4
Alexey Yarmosh <yarmoshalexey@gmail.com>
2025-04-01 16:05:05 +0200

    test: update settings e2e test

--- test/e2e/tests/settings.spec.ts
+++ test/e2e/tests/settings.spec.ts
@@ -3,8 +3,17 @@ import { test, expect } from '../fixtures.ts';
 test('Settings page', async ({ page }) => {
 	await page.goto('/settings');
 	await expect(page.locator('h1')).toHaveText('Settings');
-	await page.getByLabel('First Name').fill('Johnny');
+
+	// Make sure new token value is set to the auth.user after the save.
+	await page.getByLabel('Regenerate').click();
 	await page.getByLabel('Apply settings').click();
+
+	await page.getByLabel('First Name').fill('Johnny');
+	await page.getByLabel('johndoe').click();
+	await page.locator('#defaultPrefix_1').getByText('Scrubs').click();
+	await page.getByLabel('Regenerate').click();
+	await page.getByLabel('Apply settings').click();
+
 	await page.goto('/settings');
 	await expect(page.getByLabel('First Name')).toHaveValue('Johnny');
 });
