a77ace00453202d1376254af95a4436e03140d9d
Alex Yarmosh <yarmoshalexey@gmail.com>
2025-04-02 17:18:00 +0200

    test: logout e2e test (#73)

--- test/e2e/tests/settings.spec.ts
+++ test/e2e/tests/settings.spec.ts
@@ -17,3 +17,10 @@ test('Settings page', async ({ page }) => {
 	await page.goto('/settings');
 	await expect(page.getByLabel('First Name')).toHaveValue('Johnny');
 });
+
+test('Delete account', async ({ page }) => {
+	await page.goto('/settings');
+	await page.getByLabel('Delete account').click();
+	await page.getByRole('dialog', { name: 'Delete account' }).getByLabel('Delete account').click();
+	await expect(page.getByRole('heading', { name: 'Sign In' })).toBeVisible();
+});
