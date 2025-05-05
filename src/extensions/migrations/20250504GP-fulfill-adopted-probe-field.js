944dbcd33bf9173cd0069e154d9ba8b28a6ab0db
Alexey Yarmosh <yarmoshalexey@gmail.com>
2025-05-06 00:36:18 +0200

    feat: udpate migration

--- src/extensions/migrations/20250504GP-fulfill-adopted-probe-field.js
+++ src/extensions/migrations/20250504GP-fulfill-adopted-probe-field.js
@@ -1,20 +1,9 @@
-import Bluebird from 'bluebird';
-
 export async function up (knex) {
-	const TABLE_NAME = 'gp_credits_additions';
-	const additions = await knex(TABLE_NAME).select('*').where({ reason: 'adopted_probe' });
-
-	await Bluebird.map(additions, async (addition) => {
-		const meta = JSON.parse(addition.meta || '{}');
-
-		if (meta.id) {
-			await knex(TABLE_NAME)
-				.where('id', addition.id)
-				.update({
-					adopted_probe: meta.id,
-				});
-		}
-	}, { concurrency: 16 });
+	await knex.raw(`
+		UPDATE gp_credits_additions
+		SET adopted_probe = JSON_UNQUOTE(JSON_EXTRACT(meta, '$.id'))
+		WHERE reason = 'adopted_probe'
+	`);
 
 	console.log('Successfully updated gp_credits_additions with adopted_probe values.');
 }
