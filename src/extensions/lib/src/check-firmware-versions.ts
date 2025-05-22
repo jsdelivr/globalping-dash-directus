35c5fa2f45bbbe900ba81f35d1f4ba4c89ea4e68
Alexey Yarmosh <yarmoshalexey@gmail.com>
2025-05-22 16:58:28 +0200

    fix: search notified sw probes too

--- src/extensions/lib/src/check-firmware-versions.ts
+++ src/extensions/lib/src/check-firmware-versions.ts
@@ -9,6 +9,7 @@ type ProbeInfo = {
 	nodeVersion: string | null;
 }
 
+export const OUTDATED_SOFTWARE_NOTIFICATION_TYPE = 'outdated_software';
 export const OUTDATED_FIRMWARE_NOTIFICATION_TYPE = 'outdated_firmware';
 
 export const checkFirmwareVersions = async (probe: ProbeInfo, userId: string, context: ApiExtensionContext) => {
@@ -69,10 +70,10 @@ const sendNotificationToSoftwareProbe = async (probe: ProbeInfo, userId: string,
 		recipient: userId,
 		item: probe.id,
 		collection: 'gp_probes',
-		type: OUTDATED_FIRMWARE_NOTIFICATION_TYPE,
+		type: OUTDATED_SOFTWARE_NOTIFICATION_TYPE,
 		secondary_type: env.TARGET_NODE_VERSION,
-		subject: 'Your probe is running an outdated firmware',
-		message: `Your ${probe.name ? `probe [**${probe.name}**](/probes/${probe.id}) with IP address **${probe.ip}**` : `[probe with IP address **${probe.ip}**](/probes/${probe.id})`} is running an outdated firmware and we couldn't update it automatically. Please follow [our guide](/probes?view=update-a-probe) to update it manually.`,
+		subject: 'Your probe container is running an outdated software',
+		message: `Your ${probe.name ? `probe [**${probe.name}**](/probes/${probe.id}) with IP address **${probe.ip}**` : `[probe with IP address **${probe.ip}**](/probes/${probe.id})`} is running an outdated software and we couldn't update it automatically. Please follow [our guide](/probes?view=update-a-probe) to update it manually.`,
 	});
 
 	return probe.id;
@@ -92,7 +93,7 @@ const sendNotificationToHardwareProbe = async (probe: ProbeInfo, userId: string,
 		collection: 'gp_probes',
 		type: OUTDATED_FIRMWARE_NOTIFICATION_TYPE,
 		secondary_type: `${env.TARGET_HW_DEVICE_FIRMWARE}_${env.TARGET_NODE_VERSION}`,
-		subject: 'Your probe is running an outdated firmware',
+		subject: 'Your hardware probe is running an outdated firmware',
 		message: `Your ${probe.name ? `probe [**${probe.name}**](/probes/${probe.id}) with IP address **${probe.ip}**` : `[probe with IP address **${probe.ip}**](/probes/${probe.id})`} is running an outdated firmware and we couldn't update it automatically. Please follow [our guide](https://github.com/jsdelivr/globalping-hwprobe#download-the-latest-firmware) to update it manually.`,
 	});
 
