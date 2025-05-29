f1dff2b908828d95f679f3c1c47e14ea0f5279d2
Alexey Yarmosh <yarmoshalexey@gmail.com>
2025-05-29 16:18:13 +0200

    fix: deletion of credits additions on user delete (#86)

--- src/extensions/hooks/directus-users/src/index.ts
+++ src/extensions/hooks/directus-users/src/index.ts
@@ -1,5 +1,6 @@
 import { createError } from '@directus/errors';
 import { defineHook } from '@directus/extensions-sdk';
+import TTLCache from '@isaacs/ttlcache';
 import { getDirectusUsers, deleteCreditsAdditions, type DirectusUser } from './repositories/directus.js';
 import { validateDefaultPrefix } from './validate-fields.js';
 
@@ -7,7 +8,9 @@ export type Fields = Partial<DirectusUser>;
 
 export const UserNotFoundError = createError('UNAUTHORIZED', 'User not found.', 401);
 
-export default defineHook(({ filter }, context) => {
+export const deleteUserIdToGithubId = new TTLCache<string, string>({ ttl: 60 * 1000 });
+
+export default defineHook(({ filter, action }, context) => {
 	filter('users.update', async (payload, { keys }, { accountability }) => {
 		const fields = payload as Fields;
 
@@ -17,16 +20,13 @@ export default defineHook(({ filter }, context) => {
 	});
 
 	filter('users.delete', async (userIds, _payload, { accountability }) => {
-		if (!accountability || !accountability.user) {
-			throw new Error('User is not authenticated');
-		}
-
 		const users = await getDirectusUsers(userIds as string[], accountability, context);
+		users.forEach(user => deleteUserIdToGithubId.set(user.id, user.external_identifier));
+	});
 
-		if (!users.length) {
-			return;
-		}
-
-		await deleteCreditsAdditions(users, context);
+	action('users.delete', async (payload, { accountability }) => {
+		const userIds = (payload.keys as string[]) || [];
+		const githubIds = userIds.map(id => deleteUserIdToGithubId.get(id)).filter(Boolean) as string[];
+		await deleteCreditsAdditions(githubIds, accountability, context);
 	});
 });
