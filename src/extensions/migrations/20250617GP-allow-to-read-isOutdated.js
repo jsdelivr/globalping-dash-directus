0124b072aedd218ee5a634ef08b69068b720b5da
Alexey Yarmosh <yarmoshalexey@gmail.com>
2025-07-17 17:55:00 +0200

    feat: allow to read isOutdated

---
+++ src/extensions/migrations/20250617GP-allow-to-read-isOutdated.js
@@ -0,0 +1,15 @@
+import { getUserPermissions, editPermissions } from '../migration-utils/permissions.js';
+
+const COLLECTION_NAME = 'gp_probes';
+const FIELDS_TO_ADD = [ 'isOutdated' ];
+const FIELDS_TO_REMOVE = [];
+
+export async function up () {
+	const { readPermissions } = await getUserPermissions(COLLECTION_NAME);
+	await editPermissions(readPermissions, FIELDS_TO_ADD, FIELDS_TO_REMOVE);
+	console.log(`User read permissions patched. Added '${FIELDS_TO_ADD.join(', ')}' to the ${COLLECTION_NAME}`);
+}
+
+export async function down () {
+	console.log('There is no down operation for that migration.');
+}
