d3af2e29cfb78a2e3bcc82dfb9402d0b098671e3
Alexey Yarmosh <yarmoshalexey@gmail.com>
2025-07-24 16:43:11 +0200

    feat: add sponsorship bonuses

--- src/extensions/lib/src/add-credits.ts
+++ src/extensions/lib/src/add-credits.ts
@@ -7,6 +7,10 @@ export const SOURCE_ID_TO_TARGET_ID: Record<string, string> = {
 	203478287: '163146',
 };
 
+type CreditsAddition = {
+	meta: { amountInDollars?: number };
+};
+
 type AddCreditsData = {
 	github_id: string;
 	amount: number;
@@ -14,7 +18,35 @@ type AddCreditsData = {
 	meta: { amountInDollars: number };
 };
 
-export const addCredits = async ({ github_id, amount, reason, meta }: AddCreditsData, { services, database, getSchema, env }: ApiExtensionContext) => {
+const getUserBonus = async (githubId: string, incomingAmountInDollars: number, { services, database, getSchema, env }: ApiExtensionContext) => {
+	const { ItemsService } = services;
+
+	if (!env.BONUS_PER_100_DOLLARS_IN_LAST_YEAR || !env.MAX_CREDITS_BONUS) {
+		throw new Error('BONUS_PER_100_DOLLARS_IN_LAST_YEAR or MAX_CREDITS_BONUS was not provided');
+	}
+
+	const maxCreditsBonus = parseInt(env.MAX_CREDITS_BONUS, 10);
+
+	const creditsAdditionsService = new ItemsService('gp_credits_additions', {
+		schema: await getSchema({ database }),
+		knex: database,
+	});
+
+	const additionsInLastYear = await creditsAdditionsService.readByQuery({
+		filter: {
+			github_id: { _eq: githubId },
+			reason: { _in: [ 'recurring_sponsorship', 'one_time_sponsorship', 'tier_changed' ] },
+			date_created: { _gte: `$NOW(-1 year)` },
+		},
+	}) as CreditsAddition[];
+
+	const dollarsInLastYear = additionsInLastYear.reduce((sum, { meta }) => sum + (meta.amountInDollars ?? 0), 0);
+	const bonus = Math.floor((dollarsInLastYear + incomingAmountInDollars) / 100) * parseInt(env.BONUS_PER_100_DOLLARS_IN_LAST_YEAR, 10);
+	return bonus <= maxCreditsBonus ? bonus : maxCreditsBonus;
+};
+
+export const addCredits = async ({ github_id, amount, reason, meta }: AddCreditsData, context: ApiExtensionContext) => {
+	const { services, database, getSchema, env } = context;
 	const { ItemsService } = services;
 
 	const creditsAdditionsService = new ItemsService('gp_credits_additions', {
@@ -23,9 +55,10 @@ export const addCredits = async ({ github_id, amount, reason, meta }: AddCredits
 	});
 
 	const githubId = redirectGithubId(github_id);
+	const userBonus = await getUserBonus(githubId, amount, context);
 	const creditsId = await creditsAdditionsService.createOne({
 		github_id: githubId,
-		amount: amount * parseInt(env.CREDITS_PER_DOLLAR, 10),
+		amount: Math.floor(amount * parseInt(env.CREDITS_PER_DOLLAR, 10) * (100 + userBonus) / 100),
 		reason,
 		meta,
 	});
