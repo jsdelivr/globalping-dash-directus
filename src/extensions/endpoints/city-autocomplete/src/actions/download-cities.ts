df527d966172785e8fc4d96b6ee4eabcdbdc95a5
Alexey Yarmosh <yarmoshalexey@gmail.com>
2025-07-30 13:29:57 +0200

    fix: migrate to axios

--- src/extensions/endpoints/city-autocomplete/src/actions/download-cities.ts
+++ src/extensions/endpoints/city-autocomplete/src/actions/download-cities.ts
@@ -1,17 +1,17 @@
 import AdmZip from 'adm-zip';
-import got from 'got';
+import axios from 'axios';
 
 const URL = 'https://download.geonames.org/export/dump/cities500.zip';
 
 export const FILENAME = 'GEONAMES_CITIES.csv';
 
 const query = async (url: string): Promise<Buffer> => {
-	const result = await got(url, {
-		responseType: 'buffer',
-		timeout: { request: 5000 },
+	const result = await axios.get(url, {
+		responseType: 'arraybuffer',
+		timeout: 5000,
 	});
 
-	return result.body;
+	return result.data;
 };
 
 export const downloadCitiesFile = async (): Promise<void> => {
