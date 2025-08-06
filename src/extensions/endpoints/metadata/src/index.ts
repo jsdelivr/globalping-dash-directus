585ec44f25ec041444f34be7bf7f4f08f23842af
Alexey Yarmosh <yarmoshalexey@gmail.com>
2025-08-06 17:49:54 +0200

    feat: update /metadata

--- src/extensions/endpoints/metadata/src/index.ts
+++ src/extensions/endpoints/metadata/src/index.ts
@@ -1,27 +1,17 @@
-import { isDirectusError } from '@directus/errors';
 import { defineEndpoint } from '@directus/extensions-sdk';
+import { asyncWrapper } from '../../../lib/src/async-wrapper.js';
 
 export default defineEndpoint((router, context) => {
-	const { logger, env } = context;
+	const { env } = context;
 
-	router.get('/', async (_req, res) => {
-		try {
-			res.send({
-				targetNodeVersion: env.TARGET_NODE_VERSION,
-				targetHardwareDeviceFirmware: env.TARGET_HW_DEVICE_FIRMWARE,
-				creditsPerDollar: env.CREDITS_PER_DOLLAR,
-				creditsPerAdoptedProbe: env.CREDITS_PER_ADOPTED_PROBE_DAY,
-				creditsBonusPer100Dollars: env.CREDITS_BONUS_PER_100_DOLLARS,
-				maxCreditsBonus: env.MAX_CREDITS_BONUS,
-			});
-		} catch (error: unknown) {
-			logger.error(error);
-
-			if (isDirectusError(error)) {
-				res.status(error.status).send(error.message);
-			} else {
-				res.status(500).send('Internal Server Error');
-			}
-		}
-	});
+	router.get('/', asyncWrapper(async (_req, res) => {
+		res.send({
+			targetNodeVersion: env.TARGET_NODE_VERSION,
+			targetHardwareDeviceFirmware: env.TARGET_HW_DEVICE_FIRMWARE,
+			creditsPerDollar: env.CREDITS_PER_DOLLAR,
+			creditsPerAdoptedProbe: env.CREDITS_PER_ADOPTED_PROBE_DAY,
+			creditsBonusPer100Dollars: env.CREDITS_BONUS_PER_100_DOLLARS,
+			maxCreditsBonus: env.MAX_CREDITS_BONUS,
+		});
+	}, context));
 });
