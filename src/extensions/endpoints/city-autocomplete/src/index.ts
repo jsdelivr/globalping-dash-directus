64144dcd288aeff5d100b431fe85405a0d5adc7f
Alexey Yarmosh <yarmoshalexey@gmail.com>
2025-08-06 18:38:34 +0200

    feat: update /city-autocomplete

--- src/extensions/endpoints/city-autocomplete/src/index.ts
+++ src/extensions/endpoints/city-autocomplete/src/index.ts
@@ -1,7 +1,9 @@
-import { createError, isDirectusError } from '@directus/errors';
+import { createError } from '@directus/errors';
 import { defineEndpoint } from '@directus/extensions-sdk';
 import type { Request as ExpressRequest } from 'express';
 import Joi from 'joi';
+import { asyncWrapper } from '../../../lib/src/async-wrapper.js';
+import { validate } from '../../../lib/src/middlewares/validate.js';
 import { getCitiesIndex } from './cities-index.js';
 
 type Request = ExpressRequest & {
@@ -24,34 +26,23 @@ const cityAutocompleteSchema = Joi.object<Request>({
 }).unknown(true);
 
 export default defineEndpoint((router, context) => {
-	const { logger } = context;
 	const citiesIndex = getCitiesIndex(context);
 	citiesIndex.init().catch((err) => { throw err; });
 
-	router.get('/', async (req, res) => {
-		try {
-			const { value, error } = cityAutocompleteSchema.validate(req);
+	router.get('/', validate(cityAutocompleteSchema), asyncWrapper(async (req, res) => {
+		const { value, error } = cityAutocompleteSchema.validate(req);
 
-			if (error) {
-				throw new (createError('INVALID_PAYLOAD_ERROR', error.message, 400))();
-			}
-
-			if (!citiesIndex.isInitialized) {
-				await citiesIndex.init();
-			}
-
-			const { query, countries, limit } = value.query as unknown as { query: string; countries: string; limit: number };
-			const countriesArray = countries.split(',').map(country => country.trim()).filter(Boolean);
-			const cities = citiesIndex.searchCities(countriesArray, query, limit);
-			res.send(cities);
-		} catch (error: unknown) {
-			logger.error(error);
-
-			if (isDirectusError(error)) {
-				res.status(error.status).send(error.message);
-			} else {
-				res.status(500).send('Internal Server Error');
-			}
+		if (error) {
+			throw new (createError('INVALID_PAYLOAD_ERROR', error.message, 400))();
 		}
-	});
+
+		if (!citiesIndex.isInitialized) {
+			await citiesIndex.init();
+		}
+
+		const { query, countries, limit } = value.query as unknown as { query: string; countries: string; limit: number };
+		const countriesArray = countries.split(',').map(country => country.trim()).filter(Boolean);
+		const cities = citiesIndex.searchCities(countriesArray, query, limit);
+		res.send(cities);
+	}, context));
 });
