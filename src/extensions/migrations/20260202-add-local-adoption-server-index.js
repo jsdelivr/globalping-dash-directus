9f0a44264fb8a9da81d0d45530818f5f61d65f17
Pavel Kopecký <pavel.kopecky@icloud.com>
2026-02-24 13:29:23 +0100

    refactor: remove redundant DROP INDEX call

--- src/extensions/migrations/20260202-add-local-adoption-server-index.js
+++ src/extensions/migrations/20260202-add-local-adoption-server-index.js
@@ -1,5 +1,4 @@
 export async function up (knex) {
-	await knex.raw(`ALTER TABLE gp_probes DROP INDEX IF EXISTS gp_probes_localadoptionserver_index;`);
 	await knex.raw(`ALTER TABLE gp_probes ADD INDEX gp_probes_localadoptionserver_index (localAdoptionServer(10));`);
 
 	console.log('Index gp_probes_localadoptionserver_index added');
