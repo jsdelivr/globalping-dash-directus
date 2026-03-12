00c72e96447853d81988645cc70b13579f053304
Alexey Yarmosh <yarmoshalexey@gmail.com>
2026-03-12 13:23:50 +0100

    feat: add validation of threshold

--- src/extensions/hooks/directus-users/src/validate-fields.ts
+++ src/extensions/hooks/directus-users/src/validate-fields.ts
@@ -2,20 +2,30 @@ import { createError } from '@directus/errors';
 import type { HookExtensionContext } from '@directus/extensions';
 import type { EventContext } from '@directus/types';
 import Joi from 'joi';
-import { configurableNotificationTypes } from '../../../lib/src/notification-types.js';
+import { configurableNotifications, configurableNotificationTypes } from '../../../lib/src/notification-types.js';
 import { getDirectusUsers } from './repositories/directus.js';
 
 export const payloadError = (message: string) => new (createError('INVALID_PAYLOAD_ERROR', message, 400))();
 
+const parameterSchema = Joi.number().strict().min(0).max(1_000_000_000);
+const notificationTypesWithParameter = new Set(configurableNotificationTypes.filter(notificationType => configurableNotifications[notificationType]!.hasParameter));
+
+const createNotificationPreferenceSchema = (notificationType: string) => Joi.object({
+	enabled: Joi.boolean().required(),
+	emailEnabled: Joi.boolean().optional(),
+	parameter: notificationTypesWithParameter.has(notificationType)
+		? Joi.when('enabled', {
+			is: true,
+			then: parameterSchema.required().messages({
+				'any.required': 'Threshold value for notification should be specified.',
+			}),
+			otherwise: Joi.forbidden(),
+		})
+		: parameterSchema.optional(),
+});
+
 const userSchema = Joi.object({
-	notification_preferences: Joi.object().pattern(
-		Joi.string().max(100).valid(...configurableNotificationTypes),
-		Joi.object({
-			enabled: Joi.boolean().required(),
-			emailEnabled: Joi.boolean().optional(),
-			parameter: Joi.number().strict().min(0).max(1_000_000_000).optional(),
-		}),
-	).max(50).allow(null).optional(),
+	notification_preferences: Joi.object(Object.fromEntries(configurableNotificationTypes.map(notificationType => [ notificationType, createNotificationPreferenceSchema(notificationType) ]))).max(50).allow(null).optional(),
 }).unknown(true);
 
 export const joiValidateUser = (fields: unknown) => {
