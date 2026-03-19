2fe686fbf5a2b5403a6b942393af25594f51ad84
Alexey Yarmosh <yarmoshalexey@gmail.com>
2026-03-19 23:52:31 +0100

    feat: simplify

--- src/extensions/endpoints/local-adoption/src/index.ts
+++ src/extensions/endpoints/local-adoption/src/index.ts
@@ -5,7 +5,7 @@ import type { Request as ExpressRequest } from 'express';
 import Joi from 'joi';
 import { asyncWrapper } from '../../../lib/src/async-wrapper.js';
 import { getIpFromRequest } from '../../../lib/src/client-ip.js';
-import { type ProbeToAdopt, type Row, createAdoptedProbe, parseRow } from '../../../lib/src/create-adopted-probe.js';
+import { type Row, createAdoptedProbe, parseRow } from '../../../lib/src/create-adopted-probe.js';
 import { validate } from '../../../lib/src/middlewares/validate.js';
 
 type Request = ExpressRequest & {
@@ -76,40 +76,7 @@ export default defineEndpoint((router, context) => {
 		}
 
 		const probe = parseRow(row);
-		const probeToAdopt: ProbeToAdopt = {
-			userId: null,
-			ip: probe.ip,
-			name: null,
-			altIps: probe.altIps,
-			uuid: probe.uuid,
-			tags: [],
-			systemTags: probe.systemTags,
-			status: probe.status,
-			isIPv4Supported: probe.isIPv4Supported,
-			isIPv6Supported: probe.isIPv6Supported,
-			version: probe.version,
-			nodeVersion: probe.nodeVersion,
-			hardwareDevice: probe.hardwareDevice,
-			hardwareDeviceFirmware: probe.hardwareDeviceFirmware,
-			city: probe.city,
-			state: probe.state,
-			stateName: probe.stateName,
-			country: probe.country,
-			countryName: probe.countryName,
-			continent: probe.continent,
-			continentName: probe.continentName,
-			region: probe.region,
-			latitude: probe.latitude,
-			longitude: probe.longitude,
-			asn: probe.asn,
-			network: probe.network,
-			allowedCountries: probe.allowedCountries,
-			originalLocation: null,
-			customLocation: null,
-			localAdoptionServer: probe.localAdoptionServer,
-		};
-
-		const updatedProbe = await createAdoptedProbe(req.accountability.user!, probeToAdopt, context);
+		const updatedProbe = await createAdoptedProbe(req.accountability.user!, probe, context);
 
 		res.json(updatedProbe);
 	}, context));
