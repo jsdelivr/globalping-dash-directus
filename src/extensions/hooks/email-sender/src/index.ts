5859dbee6b984be42997e77bbaec510d51efba79
Alexey Yarmosh <yarmoshalexey@gmail.com>
2026-04-09 08:40:55 +0200

    feat: return without RESEND_API_KEY

--- src/extensions/hooks/email-sender/src/index.ts
+++ src/extensions/hooks/email-sender/src/index.ts
@@ -2,13 +2,13 @@ import { defineHook } from '@directus/extensions-sdk';
 import { getEmailService } from './email-sender.js';
 
 export default defineHook(({ action }, context) => {
+	if (!context.env.RESEND_API_KEY) {
+		return;
+	}
+
 	const emailService = getEmailService(context);
 
 	action('server.start', async () => {
-		if (!context.env.RESEND_API_KEY) {
-			return;
-		}
-
 		emailService.scheduleSend();
 	});
 
