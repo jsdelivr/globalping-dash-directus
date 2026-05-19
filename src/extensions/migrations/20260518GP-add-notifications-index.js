baefc74316697b3e6cddf4f025989cbc3de393be
Alexey Yarmosh <yarmoshalexey@gmail.com>
2026-05-19 14:23:38 +0200

    fix: create index using schema

--- src/extensions/migrations/20260518GP-add-notifications-index.js
+++
@@ -1,8 +0,0 @@
-export async function up (knex) {
-	await knex.raw(`ALTER TABLE directus_notifications ADD INDEX email_status_index (email_status);`);
-	console.log('Index "email_status_index" added');
-}
-
-export async function down () {
-	console.log('There is no down operation for that migration.');
-}
