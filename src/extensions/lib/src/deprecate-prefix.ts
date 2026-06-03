ac0a146bb6ac2fe5c076e17c303d23c658351858
Alexey Yarmosh <yarmoshalexey@gmail.com>
2026-06-03 17:11:46 +0200

    misc: update notification text

--- src/extensions/lib/src/deprecate-prefix.ts
+++ src/extensions/lib/src/deprecate-prefix.ts
@@ -61,8 +61,8 @@ export const checkDefaultPrefix = async (user: User, context: Context): Promise<
 		await notificationsService.createOne({
 			recipient: user.id,
 			type: 'default_tag_change',
-			subject: 'Action required: confirm your probe\'s default tag change',
-			message: `Your current default probe tag **u-${user.default_prefix}** is no longer valid, so it was updated to **u-${user.github_username}**.\n\nThe old **u-${user.default_prefix}** tag still works for measurement targeting for now. [Confirm the new tag](${defaultTagChangeLink}) to stop using the old one, or [choose a different tag prefix](${settingsLink}) in settings.`,
+			subject: 'Action required: confirm the default tag change',
+			message: `The current default tag **u-${user.default_prefix}** of your probes is no longer valid, so it was updated to **u-${user.github_username}**.\n\nThe old **u-${user.default_prefix}** tag still works for measurement targeting for now. [Confirm the new tag](${defaultTagChangeLink}) to stop using the old one, or [choose a different tag prefix](${settingsLink}) in settings.`,
 		});
 	});
 };
