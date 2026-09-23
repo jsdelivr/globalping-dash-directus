bb1e2a806d408607b08c6476079aaf8086240b3d
Pavel Kopecký <pavel.kopecky@icloud.com>
2026-09-23 09:55:51 +0200

    fix: align sponsorship index schema metadata

--- src/extensions/migrations/20260813GP-add-sponsorship-history-index.js
+++ src/extensions/migrations/20260813GP-add-sponsorship-history-index.js
@@ -6,10 +6,6 @@ export async function up (knex) {
 	console.log('Index gp_credits_additions_reason_date_created_index added');
 }
 
-export async function down (knex) {
-	await knex.schema.alterTable('gp_credits_additions', (table) => {
-		table.dropIndex([ 'reason', 'date_created' ], 'gp_credits_additions_reason_date_created_index');
-	});
-
-	console.log('Index gp_credits_additions_reason_date_created_index removed');
+export async function down () {
+	console.log('There is no down operation for that migration.');
 }
